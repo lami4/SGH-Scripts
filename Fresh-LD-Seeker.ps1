4416f8eef358eb1b5ed90d4545aad69fe0f410c0
lami4 <fazz@mail.ru>
2016-11-13 14:00:17 +0300

    Update Fresh-LD-Seeker.ps1

--- Fresh-LD-Seeker.ps1
+++ Fresh-LD-Seeker.ps1
@@ -1,4 +1,3 @@
-clear
 Function Select-FolderDialog
 {
     param([string]$Description="Select Folder",[string]$RootFolder="Desktop")
@@ -20,29 +19,67 @@ Function Select-FolderDialog
         }
     }
 
+    Function Make-Choice
+{
+$a = new-object -comobject wscript.shell 
+$errorBox = $a.popup("LiveDoc Report.html already exists in $PSScriptRoot!
+Do you want to overwrite it?
+Clicking 'No' will stop the script.
+",0,"Delete Files",4) 
+If ($errorBox -eq 6) {
+Remove-Item -Path "$PSScriptRoot\LiveDoc Report.html"} else {Exit} 
+}
+
 $parsedCurrentProjectPath = ($PSScriptRoot | Split-Path -Leaf) -split " "
 $projectPath = $parsedCurrentProjectPath[0]
 $table = @()
 $pathToFilesToBeTranslated = Select-FolderDialog
+$reportExistenceCheck = Test-Path "$PSScriptRoot\LiveDoc Report.html"
+if ($reportExistenceCheck -eq $true) {
+Make-Choice
+}
 $filesToBeTranslated = @(Get-ChildItem "$pathToFilesToBeTranslated" | % {$_.BaseName})
 $foldersToLoopThrough = @(Get-ChildItem "C:\Users\Анник\Desktop\# projects" | Where-Object {$_ -Match "$projectPath .*\d$"} | Sort-Object -Descending)
 
-Add-Content "$PSScriptRoot\LiveDoc Report.html" '<!doctype html>
-<html lang="en">
+Add-Content "$PSScriptRoot\LiveDoc Report.html" "<!DOCTYPE html>
+<html lang=""en"">
 
 <head>
-<meta charset="utf-8">
+<meta charset=""utf-8"">
 <title>LiveDoc Report</title>
+<style type=""text/css"">
+   div {
+    font-family: Verdana, Arial, Helvetica, sans-serif;
+   }
+
+table {
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid black;
+    padding: 3px;
+}
+td {
+    background-color: #FFC;
+}
+</style>
 </head>
 
 <body>
-<h3>Hello. Here is what I managed to find.</h3>
-<table style="width:50%" border=1px align=center>
+<div>
+<h3>Hello.</h3>
+<h3>I searched through the following folders:</h3>
+<ul style=""list-style-type:square"">"
+$foldersToLoopThrough | % {Add-Content "$PSScriptRoot\LiveDoc Report.html" "<li>$_</li>"}
+Add-Content "$PSScriptRoot\LiveDoc Report.html" "</ul>
+<h3>Here is what I managed to find.</h3>
+<table>
 <tr>
         <th>Document</th>
         <th>Status</th> 
         <th>Found In</th>
-</tr>'
+</tr>"
 
 for ($i = 0; $i -lt $filesToBeTranslated.Length; $i++)
 {
@@ -66,13 +103,14 @@ for ($i = 0; $i -lt $filesToBeTranslated.Length; $i++)
         Add-Content "$PSScriptRoot\LiveDoc Report.html" "<tr>
         <td><font color=""black"">$currentFile</font></td>
         <td><font color=""red""><b>NOT FOUND</b></font></td>
-        <td><font color=""black"">-none-</font></td>
+        <td align=""center""><font color=""black"">-none-</font></td>
 </tr>"
         }
 } 
 $table.ForEach({[PSCustomObject]$_}) | Format-Table Document, Status, FoundIn -AutoSize
 
 Add-Content "$PSScriptRoot\LiveDoc Report.html" "</table>
+</div>
 </body>
 </html>"
 Invoke-Item "$PSScriptRoot\LiveDoc Report.html"
