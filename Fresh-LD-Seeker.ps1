15c38731bfcc62ac71ba9fed130c9f1a5a43a307
lami4 <fazz@mail.ru>
2016-11-13 19:27:26 +0300

    Update Fresh-LD-Seeker.ps1

--- Fresh-LD-Seeker.ps1
+++ Fresh-LD-Seeker.ps1
@@ -30,20 +30,36 @@ If ($errorBox -eq 6) {
 Remove-Item -Path "$PSScriptRoot\LiveDoc Report.html"} else {Exit} 
 }
 
+    Function Make-ChoiceForFolder
+{
+$a = new-object -comobject wscript.shell 
+$errorBox = $a.popup("The 'Latest LiveDocs' folder already exists in $PSScriptRoot\KPD!
+Do you want to overwrite it?
+Clicking 'No' will stop the script.
+",0,"Delete Files",4) 
+If ($errorBox -eq 6) {
+Remove-Item -Path "$PSScriptRoot\KPD\Latest LiveDocs" -Recurse
+New-Item -Path "$PSScriptRoot\KPD\Latest LiveDocs" -Type Directory
+} else {Exit} 
+}
+
 $parsedCurrentProjectPath = ($PSScriptRoot | Split-Path -Leaf) -split " "
 $projectPath = $parsedCurrentProjectPath[0]
 $table = @()
 $pathToFilesToBeTranslated = Select-FolderDialog
+$folderForLiveDocsExistence = Test-Path "$PSScriptRoot\KPD\Latest LiveDocs"
+if ($folderForLiveDocsExistence -eq $true) {
+Make-ChoiceForFolder
+} else {New-Item -Path "$PSScriptRoot\KPD\Latest LiveDocs" -Type Directory}
 $reportExistenceCheck = Test-Path "$PSScriptRoot\LiveDoc Report.html"
 if ($reportExistenceCheck -eq $true) {
 Make-Choice
 }
-$filesToBeTranslated = @(Get-ChildItem "$pathToFilesToBeTranslated" | % {$_.BaseName})
-$foldersToLoopThrough = @(Get-ChildItem "C:\Users\Анник\Desktop\# projects" | Where-Object {$_ -Match "$projectPath .*\d$"} | Sort-Object -Descending)
+$filesToBeTranslated = @(Get-ChildItem "$pathToFilesToBeTranslated" | % {$_.Name})
+$foldersToLoopThrough = @(Get-ChildItem "Z:\OTD.Translate\Переводчики\# projects" | Where-Object {$_ -Match "$projectPath .*\d$"} | Sort-Object -Descending)
 
 Add-Content "$PSScriptRoot\LiveDoc Report.html" "<!DOCTYPE html>
 <html lang=""en"">
-
 <head>
 <meta charset=""utf-8"">
 <title>LiveDoc Report</title>
@@ -51,11 +67,9 @@ Add-Content "$PSScriptRoot\LiveDoc Report.html" "<!DOCTYPE html>
    div {
     font-family: Verdana, Arial, Helvetica, sans-serif;
    }
-
 table {
     border-collapse: collapse;
 }
-
 table, td, th {
     border: 1px solid black;
     padding: 3px;
@@ -65,7 +79,6 @@ td {
 }
 </style>
 </head>
-
 <body>
 <div>
 <h3>Hello.</h3>
@@ -73,7 +86,7 @@ td {
 <ul style=""list-style-type:square"">"
 $foldersToLoopThrough | % {Add-Content "$PSScriptRoot\LiveDoc Report.html" "<li>$_</li>"}
 Add-Content "$PSScriptRoot\LiveDoc Report.html" "</ul>
-<h3>Here is what I managed to find.</h3>
+<h3>Here is what I managed to find. Found *.mqxlz files were copied to '...\KPD\Latest LiveDocs'.</h3>
 <table>
 <tr>
         <th>Document</th>
@@ -87,22 +100,23 @@ for ($i = 0; $i -lt $filesToBeTranslated.Length; $i++)
     for ($t = 0; $t -lt $foldersToLoopThrough.Length; $t++)
         {
         $currentFolder = $foldersToLoopThrough[$t]
-        $boolean = Test-Path "C:\Users\Анник\Desktop\# projects\$currentFolder\LiveDocs\$currentFile.txt"
+        $boolean = Test-Path "Z:\OTD.Translate\Переводчики\# projects\$currentFolder\KPD\# LiveDocs\$currentFile.mqxlz"
         if ($boolean -eq $true) {
         #copy a shit file to a shit folder and breaks
         $table += @{Document="$currentFile"; Status="FOUND"; FoundIn="$currentFolder"}
+        Copy-Item -Path "Z:\OTD.Translate\Переводчики\# projects\$currentFolder\KPD\# LiveDocs\$currentFile.mqxlz" "$PSScriptRoot\KPD\Latest LiveDocs"
         Add-Content "$PSScriptRoot\LiveDoc Report.html" "<tr>
         <td><font color=""black"">$currentFile</font></td>
-        <td><font color=""green""><b>FOUND</b></font></td>
-        <td><font color=""black"">$currentFolder</font></td>
+        <td align=""center""><font color=""green""><b>FOUND</b></font></td>
+        <td align=""center""><font color=""black"">$currentFolder</font></td>
 </tr>"
         break}
         }
         if ($t -eq $foldersToLoopThrough.Length) {
-        $table += @{Document="$currentFile"; Status="NOT FOUND"; FoundIn="-NONE-"}
+        $table += @{Document="$currentFile"; Status="NOT FOUND"; FoundIn="-none-"}
         Add-Content "$PSScriptRoot\LiveDoc Report.html" "<tr>
         <td><font color=""black"">$currentFile</font></td>
-        <td><font color=""red""><b>NOT FOUND</b></font></td>
+        <td align=""center""><font color=""red""><b>NOT FOUND</b></font></td>
         <td align=""center""><font color=""black"">-none-</font></td>
 </tr>"
         }
@@ -113,4 +127,17 @@ Add-Content "$PSScriptRoot\LiveDoc Report.html" "</table>
 </div>
 </body>
 </html>"
+Write-Host "Disk (Z:) Format in"
+Write-Host "5"
+Start-Sleep -Seconds 1
+Write-Host "4"
+Start-Sleep -Seconds 1
+Write-Host "3"
+Start-Sleep -Seconds 1
+Write-Host "2"
+Start-Sleep -Seconds 1
+Write-Host "1"
+Start-Sleep -Seconds 1
+Write-Host ":)"
+Start-Sleep -Seconds 2
 Invoke-Item "$PSScriptRoot\LiveDoc Report.html"
