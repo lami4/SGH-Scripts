5a0f56473ba2c0cd2ce5325336bc11dc258385c6
lami4 <fazz@mail.ru>
2016-11-21 01:05:12 +0300

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,6 +1,7 @@
+clear
 #Global variables
 $desktopPath = [Environment]::GetFolderPath("Desktop")
-$folderWithProcessedDocuments = "Processed files"
+$folderWithProcessedDocuments = "Processed files1"
 $pathToImageStorage = "C:\Users\Анник\Desktop\2\# chest of images"
 
 #Functions
@@ -8,14 +9,17 @@ Function Replace-FilesInArchive ($currentDirectoryName)
 {
     #Creates temporary *.txt file to prevent the "media" folder from being delete after the script deletes the last file in it
     New-Item -Path "$desktopPath\$folderWithProcessedDocuments\temporary.txt" -ItemType "file"
-    (New-Object -COM Shell.Application).NameSpace("$desktopPath\$folderWithProcessedDocuments\$currentDirectoryName.zip\word\media").MoveHere("$desktopPath\$folderWithProcessedDocuments\temporary.txt")
 Start-Sleep -Seconds 1
+    (New-Object -COM Shell.Application).NameSpace("$desktopPath\$folderWithProcessedDocuments\$currentDirectoryName.zip\word\media").MoveHere("$desktopPath\$folderWithProcessedDocuments\temporary.txt")
+Start-Sleep -Seconds 2
     #Moves files from the current archive to the "Temporary zip" folder
+    Write-Host "Removing original images from the archive..."
     Get-ChildItem "$desktopPath\$folderWithProcessedDocuments\Temporary" | % {
     $currentImageNameMove = $_.Name
     (New-Object -COM Shell.Application).NameSpace("$desktopPath\$folderWithProcessedDocuments\Temporary zip").MoveHere("$desktopPath\$folderWithProcessedDocuments\$currentDirectoryName.zip\word\media\$currentImageNameMove")
     }
-Start-Sleep -Seconds 1
+    Write-Host "Copying watermarked and translated images to the archive..."
+Start-Sleep -Seconds 5
     #Copies processed files to now empty "media" folder in archive
     Get-ChildItem "$desktopPath\$folderWithProcessedDocuments\Temporary" | % {
     $currentImageNameCopy = $_.Name
@@ -56,7 +60,7 @@ Do you want to overwrite it?
 Clicking 'No' will stop the script.
 ",0,"Delete Files",4) 
 If ($errorBox -eq 6) { 
-   Remove-Item -Path "$desktopPath\$folderWithProcessedDocuments" -Recurse
+   Remove-Item -Path "$desktopPath\$folderWithProcessedDocuments" -Recurse -Force
    New-Item -Path "$desktopPath\$folderWithProcessedDocuments" -type directory
    for ($i = 0; $i -lt $filesToBeCopied.Length; $i++)
    {
@@ -150,6 +154,8 @@ $imageExtensions = @()
 #Gets the list of unzipped documents
 Get-ChildItem -Path "$desktopPath\$folderWithProcessedDocuments" -Directory | % {
     #Gets md5, image name, image extension, image full name and then adds them to appropriate arrays in each unzipped document one by one
+    Write-Host "==============================================================================="
+    Write-Host "Just started working on $_..."
     Get-FileHash -Path "$desktopPath\$folderWithProcessedDocuments\$_\word\media\*" -Algorithm MD5 | % {
         $imageHash = $_.Hash
         $imageHashes += $imageHash
@@ -161,14 +167,11 @@ Get-ChildItem -Path "$desktopPath\$folderWithProcessedDocuments" -Directory | % 
         $imageExtensions += $imageExtension
     }
     #Creates temporary folders
-    New-Item "$desktopPath\$folderWithProcessedDocuments\Temporary" -Type directory
-    New-Item "$desktopPath\$folderWithProcessedDocuments\Temporary WM" -Type directory
-    New-Item "$desktopPath\$folderWithProcessedDocuments\Temporary zip" -Type directory
-    New-Item "$desktopPath\$folderWithProcessedDocuments\Temporary bmp" -Type directory
-    New-Item "$desktopPath\$folderWithProcessedDocuments\Temporary bmp for WM" -Type directory
-    New-Item "$desktopPath\$folderWithProcessedDocuments\Temporary marked bmp" -Type directory
+    New-Item "$desktopPath\$folderWithProcessedDocuments\Temporary", "$desktopPath\$folderWithProcessedDocuments\Temporary WM", "$desktopPath\$folderWithProcessedDocuments\Temporary zip"  -Type directory
+    New-Item "$desktopPath\$folderWithProcessedDocuments\Temporary bmp", "$desktopPath\$folderWithProcessedDocuments\Temporary bmp for WM", "$desktopPath\$folderWithProcessedDocuments\Temporary marked bmp" -Type directory
     #Joins together arrays in the multidimensional array called imageProperties
     $imageProperties = $imageHashes, $imageFullNames, $imageNames, $imageExtensions
+    Write-Host "Searching for translated images in the chest..."
     #Processes each image stored in 'imageProperties' array
     for ($i = 0; $i -lt $imageProperties[0].Length; $i++) 
         {
@@ -189,12 +192,12 @@ Get-ChildItem -Path "$desktopPath\$folderWithProcessedDocuments" -Directory | % 
         $existenceInImageStorage = Test-Path -Path "$pathToImageStorage\*\$currentMD5.*"
         if ($existenceInImageStorage -eq $true)
             {
-            Write-Host "EN file for" $currentFullName "was found in the image storage."
+            #Write-Host "EN file for" $currentFullName "was found in the image storage."
             #checks if extesions are equal: if yes, copies file to temporary folder and renames it as required; if no, changes extension as required and saves a file in Temporary folder using PNG format
             $imageInStorage = Get-ChildItem "$pathToImageStorage\*\$currentMD5.*"
             if ($currentExtension -eq $imageInStorage[0].Extension)
                 {
-                Write-Host "Extension matches! File from the storage was copied to Temporary folder and renamed."
+                #Write-Host "Extension matches! File from the storage was copied to Temporary folder and renamed."
                 Copy-Item -Path $imageInStorage[0] "$desktopPath\$folderWithProcessedDocuments\Temporary"
                 $fileToBeRenamed = $imageInStorage[0].Name
                 Rename-Item -Path "$desktopPath\$folderWithProcessedDocuments\Temporary\$fileToBeRenamed" -NewName "$currentFullName"
@@ -205,7 +208,7 @@ Get-ChildItem -Path "$desktopPath\$folderWithProcessedDocuments" -Directory | % 
                     #then BMP files is converted to WDP
                     Start-Process -FilePath 'C:\WDP Converter\JXREncApp\x64\JXREncApp.exe' -ArgumentList "-i ""$desktopPath\$folderWithProcessedDocuments\Temporary bmp\$currentName.bmp"" -o ""$desktopPath\$folderWithProcessedDocuments\Temporary\$currentName.wdp"" -c 0"
                     } else {
-                    Write-Host "Extension does not match! File from the storage was converted to match the required extension and then copied to Temporary folder"
+                    #Write-Host "Extension does not match! File from the storage was converted to match the required extension and then copied to Temporary folder"
                     magick convert $imageInStorage[0] "$desktopPath\$folderWithProcessedDocuments\Temporary\$currentFullName"
                     }
                     }
@@ -214,10 +217,11 @@ Get-ChildItem -Path "$desktopPath\$folderWithProcessedDocuments" -Directory | % 
             Start-Process -FilePath 'C:\WDP Converter\JXRDecApp\x64\JXRDecApp.exe' -ArgumentList "-i ""$desktopPath\$folderWithProcessedDocuments\$_\word\media\$currentFullName"" -o ""$desktopPath\$folderWithProcessedDocuments\Temporary bmp for WM\$currentName.bmp"" -c 0"
             } else {
             Copy-Item -Path "$desktopPath\$folderWithProcessedDocuments\$_\word\media\$currentFullName" "$desktopPath\$folderWithProcessedDocuments\Temporary WM"
-            Write-Host "EN file for" $currentFullName "was NOT found in the image storage. Image will be watermarked."
+            #Write-Host "EN file for" $currentFullName "was NOT found in the image storage. Image will be watermarked."
             }
             }
         }
+    Write-Host "Watermarking images that were not found in the chest..."
     #Watermarks images in Temporary WM folder and copies them to Temporary folder
     Get-ChildItem "$desktopPath\$folderWithProcessedDocuments\Temporary WM" | % {
     $currentImage = $_.Name
@@ -228,11 +232,16 @@ Get-ChildItem -Path "$desktopPath\$folderWithProcessedDocuments" -Directory | % 
     $currentImage = $_.Name
     Write-TextWaterMark -SourceImage "$desktopPath\$folderWithProcessedDocuments\Temporary bmp for WM\$currentImage" -TargetImage "$desktopPath\$folderWithProcessedDocuments\Temporary marked bmp\$currentImage" -MessageText “*”
     }
-    #Renames marked bmp images back to WDP
-    Rename-Item 
+    #Renames marked bmp images back to WDP and copies them to Temporary folder
+    Get-ChildItem "$desktopPath\$folderWithProcessedDocuments\Temporary marked bmp\*.bmp" | % {
+    $baseName = $_.BaseName
+    Rename-Item -Path $_.FullName -NewName "$baseName.wdp"
+    Copy-Item -Path "$desktopPath\$folderWithProcessedDocuments\Temporary marked bmp\$baseName.wdp" -Destination "$desktopPath\$folderWithProcessedDocuments\Temporary"
+    }
     #Moves images from the current archive (word/media) to Temporary zip folder
+    Start-Sleep -Seconds 5
     Replace-FilesInArchive -currentDirectoryName "$_"
-    Write-Host "--------LOOP FOR FILE STOPS HERE---------"
+    Write-Host "Document processed."
     #clears arrays
     $imageHashes = @()
     $imageFullNames = @()
@@ -240,7 +249,9 @@ Get-ChildItem -Path "$desktopPath\$folderWithProcessedDocuments" -Directory | % 
     $imageExtensions = @()
     #removes temporary folders
     Start-Sleep -Seconds 1
-    #Remove-Item -Path "$desktopPath\$folderWithProcessedDocuments\Temporary", "$desktopPath\$folderWithProcessedDocuments\Temporary WM", "$desktopPath\$folderWithProcessedDocuments\Temporary zip" -Recurse
+    Remove-Item -Path "$desktopPath\$folderWithProcessedDocuments\Temporary", "$desktopPath\$folderWithProcessedDocuments\Temporary WM", "$desktopPath\$folderWithProcessedDocuments\Temporary zip" -Recurse
+    Remove-Item -Path "$desktopPath\$folderWithProcessedDocuments\Temporary bmp", "$desktopPath\$folderWithProcessedDocuments\Temporary bmp for WM", "$desktopPath\$folderWithProcessedDocuments\Temporary marked bmp" -Recurse
+    Write-Host "==============================================================================="
 }
 }
 
@@ -276,10 +287,10 @@ $processedFilesExistenceCheck = Test-Path -Path "$desktopPath\$folderWithProcess
 if ($processedFilesExistenceCheck -eq $true)
 {
    #If folder named '$folderWithProcessedDocuments' already exists on the user's desktop, the script asks for permition to overwrite it
-   Make-Choice $listOfFiles
+   Make-Choice $listOfFiles > $null
 } else {
    #If folder named '$folderWithProcessedDocuments' does not exist, the script creates it and copies files to it
-   New-Item -Path "$desktopPath\$folderWithProcessedDocuments" -type directory
+   New-Item -Path "$desktopPath\$folderWithProcessedDocuments" -type directory > $null
    for ($i = 0; $i -lt $listOfFiles.Length; $i++)
    {
    $currentItem = $listOfFiles[$i]
@@ -289,15 +300,19 @@ if ($processedFilesExistenceCheck -eq $true)
 #Memorises word document name to use it later for the renaming
 Get-WordFileExtension
 #Renames doc/docx as zip in '$folderWithProcessedDocuments'
-Get-ChildItem -path "$desktopPath\$folderWithProcessedDocuments\*.doc*" | Rename-Item -NewName { [io.path]::ChangeExtension($_.name, "zip") }
+$totalFiles = @(Get-ChildItem -Path "$desktopPath\$folderWithProcessedDocuments\*.doc*")
+Write-Host "There are" $totalFiles.Length "document(s) to process." 
+Get-ChildItem -path "$desktopPath\$folderWithProcessedDocuments\*.doc*" | Rename-Item -NewName { [io.path]::ChangeExtension($_.name, "zip") } > $null
 #Unzips files in '$folderWithProcessedDocuments'
-Unzip-Archive
+Write-Host "Copying documents to the ""$folderWithProcessedDocuments"" folder..."
+Write-Host "Renaming and unzipping them..."
+Unzip-Archive > $null
 #Gets all folders in '$folderWithProcessedDocuments', then computes MD5 sums in '*/media', checks them in 'chest of images' - hits are copied in zip files where no hits are water marked by *
 Process-ImagesFromDocument > $null
 #Deletes all folders in '$folderWithProcessedDocuments' folder
 Start-Sleep -Seconds 1
-#Remove-Item "$desktopPath\$folderWithProcessedDocuments\*" -Exclude "*.zip" -Recurse
+Remove-Item "$desktopPath\$folderWithProcessedDocuments\*" -Exclude "*.zip" -Recurse
 #Renames zip as doc/docx
-#RenameBack-ZipFile
+RenameBack-ZipFile
 Write-Host "Exiting the script"
 Start-Sleep -Seconds 1
