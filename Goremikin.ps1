9e7fed91952c0b4f18c0e6a544778bdbbdb1a645
lami4 <fazz@mail.ru>
2017-01-04 00:49:26 +0300

    Update Goremikin.ps1

--- Goremikin.ps1
+++ Goremikin.ps1
@@ -6,3 +6,33 @@ $newvalue = $valueInCell -replace '\.', ' ' -replace '\s+', ' ' -replace 'ё', '
 Write-Host $newvalue.ToLower()
 $document.Close()
 $word.Quit()
+
+#Newest
+$documentTitles = @()
+$documentNames = @()
+$word = New-Object -ComObject Word.Application
+$word.Visible = $false
+$document = $word.Documents.Open("C:\Users\Светлана\Desktop\test\13.docx")
+[int]$rowCount = $document.Tables.Item(1).Rows.Count
+for ($i = 0; $i -lt $rowCount; $i++) {
+#takes value for document title
+[string]$valueInDocumentTitleCell = $document.Tables.Item(1).Cell($i,2).Range.Text
+$parsedDocumentTitleValue = $valueInDocumentTitleCell -replace '\.', ' ' -replace '\s+', ' ' -replace 'ё', 'е'
+    if ($valueInDocumentTitleCell.Length -le 2) {
+    Write-Host "Empty Cell"
+    } else {
+    $documentTitles += $parsedDocumentTitleValue.ToLower()
+    }
+#takes value for document title
+[string]$valueInDocumentNameCell = $document.Tables.Item(1).Cell($i,1).Range.Text
+$parsedDocumentNameValue = $valueInDocumentNameCell -replace '\s+', ' '
+    if ($parsedDocumentNameValue.Length -le 2) {
+    Write-Host "Empty Cell"
+    } else {
+    $documentNames += $valueInDocumentNameCell
+    }
+}
+$document.Close()
+$word.Quit()
+Write-Host $documentTitles
+Write-Host $documentNames
