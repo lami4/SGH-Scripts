4d551f90c62b087008baf2dae80f5f19164270b5
lami4 <fazz@mail.ru>
2017-01-27 01:15:12 +0300

    Update Check-Changes.ps1

--- Check-Changes.ps1
+++ Check-Changes.ps1
@@ -5,15 +5,19 @@ $book = $excel.Workbooks.Open("Z:\OTD.Translate\Учет программ и П�
 $sheet = $book.WorkSheets.Item("Лист1")
 $range = $sheet.Range("E:E")
 $target = $range.Find("PABKRF-GL-RU-00.00.00.dRNT.01.00")
-$firstHit = $target
-Do
-{
-    $changeAddress = $target.AddressLocal($false, $false) -replace "E", ""
-    [int]$valueInCell = $sheet.Cells.Item($changeAddress, "J").Value()
-    if ($valueInCell.Count -eq 0) {$changes += 0} else {$changes += $valueInCell}
-    $target = $Range.FindNext($target)
+if ($target -eq $null) {
+    Write-Host "No match found"
+    } else {
+    $firstHit = $target
+    Do
+    {
+        $changeAddress = $target.AddressLocal($false, $false) -replace "E", ""
+        [int]$valueInCell = $sheet.Cells.Item($changeAddress, "J").Value()
+        if ($valueInCell.Count -eq 0) {$changes += 0} else {$changes += $valueInCell}
+        $target = $Range.FindNext($target)
+    }
+    While ($target -ne $NULL -and $target.AddressLocal() -ne $firstHit.AddressLocal())
+    $greatestValue = $changes | Measure-Object -Maximum
+    Write-Host $changes
+    Write-Host $greatestValue.Maximum
 }
-While ($target -ne $NULL -and $target.AddressLocal() -ne $firstHit.AddressLocal())
-$greatestValue = $changes | Measure-Object -Maximum
-Write-Host $changes
-Write-Host $greatestValue.Maximum
