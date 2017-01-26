d6b23f8af454f83a24e576d2b8d6617990c20ae2
lami4 <fazz@mail.ru>
2017-01-27 00:58:08 +0300

    Create Check-Changes.ps1

---
+++ Check-Changes.ps1
@@ -0,0 +1,19 @@
+$changes = @()
+$excel = new-object -com excel.application
+$excel.Visible = $true
+$book = $excel.Workbooks.Open("Z:\OTD.Translate\Учет программ и ПД.xls")
+$sheet = $book.WorkSheets.Item("Лист1")
+$range = $sheet.Range("E:E")
+$target = $range.Find("PABKRF-GL-RU-00.00.00.dRNT.01.00")
+$firstHit = $target
+Do
+{
+    $changeAddress = $target.AddressLocal($false, $false) -replace "E", ""
+    [int]$valueInCell = $sheet.Cells.Item($changeAddress, "J").Value()
+    if ($valueInCell.Count -eq 0) {$changes += 0} else {$changes += $valueInCell}
+    $target = $Range.FindNext($target)
+}
+While ($target -ne $NULL -and $target.AddressLocal() -ne $firstHit.AddressLocal())
+$greatestValue = $changes | Measure-Object -Maximum
+Write-Host $changes
+Write-Host $greatestValue.Maximum
