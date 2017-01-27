2ee55b203e93010ed9414b979e780e27c89b3af1
lami4 <fazz@mail.ru>
2017-01-27 10:23:52 +0300

    Update Check-Changes.ps1

--- Check-Changes.ps1
+++ Check-Changes.ps1
@@ -3,6 +3,7 @@ $excel = new-object -com excel.application
 $excel.Visible = $true
 $book = $excel.Workbooks.Open("Z:\OTD.Translate\Учет программ и ПД.xls")
 $sheet = $book.WorkSheets.Item("Лист1")
+#снимать все фильтры
 $range = $sheet.Range("E:E")
 $target = $range.Find("PABKRF-GL-RU-00.00.00.dRNT.01.00")
 if ($target -eq $null) {
@@ -14,7 +15,7 @@ if ($target -eq $null) {
         $changeAddress = $target.AddressLocal($false, $false) -replace "E", ""
         [int]$valueInCell = $sheet.Cells.Item($changeAddress, "J").Value()
         if ($valueInCell.Count -eq 0) {$changes += 0} else {$changes += $valueInCell}
-        $target = $Range.FindNext($target)
+        $target = $range.FindNext($target)
     }
     While ($target -ne $NULL -and $target.AddressLocal() -ne $firstHit.AddressLocal())
     $greatestValue = $changes | Measure-Object -Maximum
