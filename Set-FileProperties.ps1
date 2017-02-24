c0f5d7233919608de1619b13e10aa707f8a41bd1
lami4 <fazz@mail.ru>
2017-02-24 19:14:49 +0300

    Update Set-FileProperties.ps1

--- Set-FileProperties.ps1
+++ Set-FileProperties.ps1
@@ -1,16 +1,36 @@
-clear
 $files = @()
+#Functions
 Function Select-File {
 Add-Type -AssemblyName System.Windows.Forms
 $f = new-object Windows.Forms.OpenFileDialog
-$f.InitialDirectory = "C:\Users\Светлана\Desktop"
+$f.InitialDirectory = "$PSScriptRoot"
 $f.Filter = "MS Excel Files (*.xls*)|*.xls*|All Files (*.*)|*.*"
 $f.ShowHelp = $false
 $f.Multiselect = $true
 $show = $f.ShowDialog()
 If ($show -eq "OK") {if ($f.Multiselect) { $f.FileNames } else { $f.FileName }} else {Exit}
 }
+
+Function Select-Folder ($description)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null     
+    $objForm = New-Object System.Windows.Forms.FolderBrowserDialog
+    $objForm.Rootfolder = "Desktop"
+    $objForm.Description = $description
+    $Show = $objForm.ShowDialog()
+        If ($Show -eq "OK") {
+        Return $objForm.SelectedPath
+        } Else {
+        Exit
+        }
+}
+
+$selectedFolder = Select-Folder -description "Select folder with files whose properties are to be changed"
 $selectedFile = Select-File
+#word
+$application = New-Object -ComObject word.application
+$application.Visible = $false
+#excel
 $excel = New-Object -ComObject Excel.Application
 $excel.Visible = $false
 $workbook = $excel.WorkBooks.Open($selectedFile)
@@ -22,26 +42,40 @@ for ($i = 2; $i -le $lastNonemptyCellInColumn; $i++) {
 [string]$valueInCell = $worksheet.Cells.Item($i, "D").Value()
 $files += $valueInCell
 }
-for ($i = 0; $i -le $files.Count; $i++) {
-    $range = $worksheet.Range("C2:C$lastNonemptyCellInColumn")
+for ($i = 0; $i -lt $files.Count; $i++) {
+    $currentFileName = $files[$i]
+    $document = $application.documents.open("$selectedFolder\$currentFileName")
+    $range = $worksheet.Range("C:C")
     $target = $range.Find($files[$i])
         if ($target -eq $null) {
-        Write-Host "No match found"
+        Write-Host "No properties for" $files[$i]
         } else {
         $firstHit = $target
         Do
         {
-        $changeAddress = $target.AddressLocal($false, $false) -replace "E", ""
-        [int]$valueInCell = $sheet.Cells.Item($changeAddress, "J").Value()
-        if ($valueInCell.Count -eq 0) {$changes += 0} else {$changes += $valueInCell}
+    Write-Host "Value found ("$target.AddressLocal()")"
+    $currentAddress = $target.AddressLocal($false, $false) -replace "C", ""
+    $propertyName = $worksheet.Cells.Item($currentAddress, "A").Value()
+    Write-Host "Name:" $propertyName
+    $propertyValue = $worksheet.Cells.Item($currentAddress, "B").Value()
+    Write-Host "Value:" $propertyValue
+    $propertyType = $worksheet.Cells.Item($currentAddress, "E").Value()
+    Write-Host "Type:" $propertyType
+        if ($propertyType -eq "B") {
+        #set new translated values for BuiltInProperties
+        Write-Host "This is B"
+        } else {
+        #set new translated values for CustomProperties
+        Write-Host "This is C"
+        }
         $target = $range.FindNext($target)
         }
-    While ($target -ne $NULL -and $target.AddressLocal() -ne $firstHit.AddressLocal())
-    $greatestValue = $changes | Measure-Object -Maximum
-    Write-Host $changes
-    Write-Host $greatestValue.Maximum
-    }
+        While ($target.AddressLocal() -ne $firstHit.AddressLocal())
+        }
+Write-Host "End of document"
+$document.Close()
 }
 Write-Host $files
 $workbook.Close()
 $excel.Quit()
+$application.quit()
