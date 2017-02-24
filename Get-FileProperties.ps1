7472578157f44db9caf13cb53bc378d7966bdbeb
lami4 <fazz@mail.ru>
2017-02-24 03:34:50 +0300

    Update Get-FileProperties.ps1

--- Get-FileProperties.ps1
+++ Get-FileProperties.ps1
@@ -14,7 +14,8 @@ Function Select-Folder ($description)
 }
 
 $selectedPath = Select-Folder
-$row = 1
+$row = 2
+$listOfFiles = 2
 $blacklist= @("Template", "Security", "Revision number", "Application name", "Last print date", "Number of bytes", "Number of characters (with spaces)", "Number of multimedia clips", "Number of hidden Slides", "Number of notes", "Number of slides", "Number of paragraphs", "Number of lines", "Number of characters", "Number of words", "Number of pages", "Total editing time", "Last save time", "Creation date")
 #excel
 $excel = New-Object -ComObject Excel.Application
@@ -25,6 +26,10 @@ $worksheet = $workbook.Worksheets.Item(1)
 $application = New-Object -ComObject word.application
 $application.Visible = $false
 $worksheet.Range("B:B").NumberFormat = "@"
+$worksheet.Cells.Item(1, 1) = "Property name"
+$worksheet.Cells.Item(1, 2) = "Property value"
+$worksheet.Cells.Item(1, 3) = "Property holder"
+$worksheet.Cells.Item(1, 4) = "Processed files"
 Get-ChildItem -Path "$selectedPath\*.*" -Include "*.doc*" | % {
 Write-Host "Taking properties from" $_.Name
 $document = $application.documents.open($_.FullName)
@@ -66,6 +71,8 @@ $pn = [System.__ComObject].InvokeMember(“name”,$binding::GetProperty,$null,$
     $row += 1
     }
 }
+$worksheet.Cells.Item($listOfFiles, 4) = $_.Name
+$listOfFiles += 1
 Write-Host "------End of document-----"
 $document.Close()
 }
@@ -76,3 +83,14 @@ $application.quit()
 $workbook.SaveAs("$PSScriptRoot\Properties.xlsx")
 $workbook.Close()
 $excel.Quit()
+<#
+$excel = New-Object -ComObject Excel.Application
+$excel.Visible = $false
+$workbook = $excel.WorkBooks.Open("C:\Users\Светлана\Desktop\Properties.xlsx")
+$worksheet = $workbook.Worksheets.Item(1)
+$xlup = -4121
+$kek = $worksheet.Range("B2").End($xlup).row
+Write-Host $kek
+$workbook.Close()
+$excel.Quit()
+#>
