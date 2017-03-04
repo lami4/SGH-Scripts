ba7e29432b5dd0e116de148ccb500fddc646f381
lami4 <fazz@mail.ru>
2017-03-05 02:20:54 +0300

    Update Form.ps1

--- Form.ps1
+++ Form.ps1
@@ -1,4 +1,3 @@
-clear
 $dialog = New-Object System.Windows.Forms.Form
 $dialog.ShowIcon = $false
 $dialog.AutoSize = $true
@@ -24,6 +23,7 @@ $SystemWindowsFormsMargin = New-Object System.Windows.Forms.Padding
 $SystemWindowsFormsMargin.Bottom = 25
 $buttonRunScript.Margin = $SystemWindowsFormsMargin
 $buttonRunScript.Add_Click($onClickRunScript)
+$buttonRunScript.Enabled = $false
 #Exit
 $buttonExit = New-Object System.Windows.Forms.Button
 $buttonExit.Height = 30
@@ -46,6 +46,7 @@ $SystemDrawingPoint = New-Object System.Drawing.Point
 $SystemDrawingPoint.X = 25
 $SystemDrawingPoint.Y = 25
 $checkboxTranslateBuiltInProperties.Location = $SystemDrawingPoint
+$checkboxTranslateBuiltInProperties.Add_CheckStateChanged({if ($checkboxRunLWIDBmacro.Checked -or $checkboxTranslateBuiltInProperties.Checked -or $checkboxTranslateCustomProperties.Checked -or $checkboxUpdateFieldsBody.Checked -or $checkboxUpdateFieldsFooterHeader.Checked -or $checkboxUpdateTOC.Checked -or $checkboxUnhideHiddenText.Checked -or $checkboxRunATFmacro.Checked) {$buttonRunScript.Enabled = $true} else {$buttonRunScript.Enabled = $false}})
 #Translate Custom Properties
 $checkboxTranslateCustomProperties = New-Object System.Windows.Forms.CheckBox
 $checkboxTranslateCustomProperties.Width = 350
@@ -54,6 +55,7 @@ $SystemDrawingPoint = New-Object System.Drawing.Point
 $SystemDrawingPoint.X = 25
 $SystemDrawingPoint.Y = 50
 $checkboxTranslateCustomProperties.Location = $SystemDrawingPoint
+$checkboxTranslateCustomProperties.Add_CheckStateChanged({if ($checkboxRunLWIDBmacro.Checked -or $checkboxTranslateBuiltInProperties.Checked -or $checkboxTranslateCustomProperties.Checked -or $checkboxUpdateFieldsBody.Checked -or $checkboxUpdateFieldsFooterHeader.Checked -or $checkboxUpdateTOC.Checked -or $checkboxUnhideHiddenText.Checked -or $checkboxRunATFmacro.Checked) {$buttonRunScript.Enabled = $true} else {$buttonRunScript.Enabled = $false}})
 #Update Fields in Document Body
 $checkboxUpdateFieldsBody = New-Object System.Windows.Forms.CheckBox
 $checkboxUpdateFieldsBody.Width = 350
@@ -62,6 +64,7 @@ $SystemDrawingPoint = New-Object System.Drawing.Point
 $SystemDrawingPoint.X = 25
 $SystemDrawingPoint.Y = 75
 $checkboxUpdateFieldsBody.Location = $SystemDrawingPoint
+$checkboxUpdateFieldsBody.Add_CheckStateChanged({if ($checkboxRunLWIDBmacro.Checked -or $checkboxTranslateBuiltInProperties.Checked -or $checkboxTranslateCustomProperties.Checked -or $checkboxUpdateFieldsBody.Checked -or $checkboxUpdateFieldsFooterHeader.Checked -or $checkboxUpdateTOC.Checked -or $checkboxUnhideHiddenText.Checked -or $checkboxRunATFmacro.Checked) {$buttonRunScript.Enabled = $true} else {$buttonRunScript.Enabled = $false}})
 #Update Fields in Document Footers and Headers
 $checkboxUpdateFieldsFooterHeader = New-Object System.Windows.Forms.CheckBox
 $checkboxUpdateFieldsFooterHeader.Width = 350
@@ -70,6 +73,7 @@ $SystemDrawingPoint = New-Object System.Drawing.Point
 $SystemDrawingPoint.X = 25
 $SystemDrawingPoint.Y = 100
 $checkboxUpdateFieldsFooterHeader.Location = $SystemDrawingPoint
+$checkboxUpdateFieldsFooterHeader.Add_CheckStateChanged({if ($checkboxRunLWIDBmacro.Checked -or $checkboxTranslateBuiltInProperties.Checked -or $checkboxTranslateCustomProperties.Checked -or $checkboxUpdateFieldsBody.Checked -or $checkboxUpdateFieldsFooterHeader.Checked -or $checkboxUpdateTOC.Checked -or $checkboxUnhideHiddenText.Checked -or $checkboxRunATFmacro.Checked) {$buttonRunScript.Enabled = $true} else {$buttonRunScript.Enabled = $false}})
 #Update Table of Content in Documents
 $checkboxUpdateTOC = New-Object System.Windows.Forms.CheckBox
 $checkboxUpdateTOC.Width = 350
@@ -78,6 +82,7 @@ $SystemDrawingPoint = New-Object System.Drawing.Point
 $SystemDrawingPoint.X = 25
 $SystemDrawingPoint.Y = 125
 $checkboxUpdateTOC.Location = $SystemDrawingPoint
+$checkboxUpdateTOC.Add_CheckStateChanged({if ($checkboxRunLWIDBmacro.Checked -or $checkboxTranslateBuiltInProperties.Checked -or $checkboxTranslateCustomProperties.Checked -or $checkboxUpdateFieldsBody.Checked -or $checkboxUpdateFieldsFooterHeader.Checked -or $checkboxUpdateTOC.Checked -or $checkboxUnhideHiddenText.Checked -or $checkboxRunATFmacro.Checked) {$buttonRunScript.Enabled = $true} else {$buttonRunScript.Enabled = $false}})
 #Unhide Hidden Text
 $checkboxUnhideHiddenText = New-Object System.Windows.Forms.CheckBox
 $checkboxUnhideHiddenText.Width = 350
@@ -86,6 +91,7 @@ $SystemDrawingPoint = New-Object System.Drawing.Point
 $SystemDrawingPoint.X = 25
 $SystemDrawingPoint.Y = 150
 $checkboxUnhideHiddenText.Location = $SystemDrawingPoint
+$checkboxUnhideHiddenText.Add_CheckStateChanged({if ($checkboxRunLWIDBmacro.Checked -or $checkboxTranslateBuiltInProperties.Checked -or $checkboxTranslateCustomProperties.Checked -or $checkboxUpdateFieldsBody.Checked -or $checkboxUpdateFieldsFooterHeader.Checked -or $checkboxUpdateTOC.Checked -or $checkboxUnhideHiddenText.Checked -or $checkboxRunATFmacro.Checked) {$buttonRunScript.Enabled = $true} else {$buttonRunScript.Enabled = $false}})
 #Run "ApplyTitleFormatting" macro
 $checkboxRunATFmacro = New-Object System.Windows.Forms.CheckBox
 $checkboxRunATFmacro.Width = 350
@@ -94,6 +100,7 @@ $SystemDrawingPoint = New-Object System.Drawing.Point
 $SystemDrawingPoint.X = 25
 $SystemDrawingPoint.Y = 175
 $checkboxRunATFmacro.Location = $SystemDrawingPoint
+$checkboxRunATFmacro.Add_CheckStateChanged({if ($checkboxRunLWIDBmacro.Checked -or $checkboxTranslateBuiltInProperties.Checked -or $checkboxTranslateCustomProperties.Checked -or $checkboxUpdateFieldsBody.Checked -or $checkboxUpdateFieldsFooterHeader.Checked -or $checkboxUpdateTOC.Checked -or $checkboxUnhideHiddenText.Checked -or $checkboxRunATFmacro.Checked) {$buttonRunScript.Enabled = $true} else {$buttonRunScript.Enabled = $false}})
 #Run "LocateWatermarksInDocumentBody" macro
 $checkboxRunLWIDBmacro = New-Object System.Windows.Forms.CheckBox
 $checkboxRunLWIDBmacro.Width = 350
@@ -102,9 +109,9 @@ $SystemDrawingPoint = New-Object System.Drawing.Point
 $SystemDrawingPoint.X = 25
 $SystemDrawingPoint.Y = 200
 $checkboxRunLWIDBmacro.Location = $SystemDrawingPoint
+$checkboxRunLWIDBmacro.Add_CheckStateChanged({if ($checkboxRunLWIDBmacro.Checked -or $checkboxTranslateBuiltInProperties.Checked -or $checkboxTranslateCustomProperties.Checked -or $checkboxUpdateFieldsBody.Checked -or $checkboxUpdateFieldsFooterHeader.Checked -or $checkboxUpdateTOC.Checked -or $checkboxUnhideHiddenText.Checked -or $checkboxRunATFmacro.Checked) {$buttonRunScript.Enabled = $true} else {$buttonRunScript.Enabled = $false}})
 #Event handler
 $onClickRunScript = {if ($checkboxTranslateBuiltInProperties.Checked -or $checkboxTranslateCustomProperties.Checked) {Write-Host "Script started"} else {Write-Host "Script cannot be started"}}
-
 $dialog.Controls.Add($checkboxTranslateBuiltInProperties)
 $dialog.Controls.Add($checkboxTranslateCustomProperties)
 $dialog.Controls.Add($checkboxUpdateFieldsBody)
