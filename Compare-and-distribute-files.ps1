5c2ff6776bf69a767dfac7b93bafc11bff0a2cba
lami4 <fazz@mail.ru>
2017-03-09 15:04:43 +0300

    Update Compare-and-distribute-files.ps1

--- Compare-and-distribute-files.ps1
+++ Compare-and-distribute-files.ps1
@@ -1,8 +1,10 @@
+#global arrays and variables
 $script:yesNoUserInput = 0
-#global arrays
 $fileNames = @()
 $fileMD5s = @()
 $fileFullNames = @()
+$script:SourceDocumentsToBeTranslated = 0
+$script:MathcesAgainstPreviousVersion = 0
 #functions
 Function Select-Folder ($description)
 {
@@ -30,22 +32,26 @@ Function Prompt-Overwrite ($FileName, $TestPath, $FileFullName, $Statistics) {
 $check = Test-Path -Path "$TestPath\$FileName"
     if ($check -eq $false) {
     Copy-Item -Path $FileFullName -Destination "$TestPath"
-    Add-Content -Path "$PSScriptRoot\Comparison Report.html" -Value "        <td>Copied to '$Statistics'</td>
+    Add-Content -Path "$PSScriptRoot\Comparison Report.html" -Value "        <td id=""action"">Copied to '$Statistics'</td>
 </tr>" -Encoding UTF8
     } else {
-    Input-YesOrNo -Question "$FileName already exists in $TestPath. Do you want to overwrite it?"
+    $nl = [System.Environment]::NewLine
+    Input-YesOrNo -Question "$FileName already exists in $TestPath.$nl`Do you want to overwrite it?"
         if ($script:yesNoUserInput -eq 1) {
         Copy-Item -Path $FileFullName -Destination "$TestPath"
-        Add-Content -Path "$PSScriptRoot\Comparison Report.html" -Value "        <td>Copied to '$Statistics'</td>
+        Add-Content -Path "$PSScriptRoot\Comparison Report.html" -Value "        <td id=""action"">Copied to '$Statistics'</td>
 </tr>" -Encoding UTF8
         } else {
-        Add-Content -Path "$PSScriptRoot\Comparison Report.html" -Value "        <td>Copying to '$Statistics' failed.<br>Overwrite cancelled by user.</td>
+        Add-Content -Path "$PSScriptRoot\Comparison Report.html" -Value "        <td id=""action"">Copying to '$Statistics' failed.<br>Overwrite cancelled by user.</td>
 </tr>" -Encoding UTF8
 }
     $script:yesNoUserInput = 0
     }
 }
 
+#script code
+$path = Select-Folder -description "Specify a path to '# Source documents doc, docx, xls, xlsx' folder you want to compare new documents against"
+Write-Host "Calculating MD5 checksums..."
 #==========Statistics==========#
 Add-Content -Path "$PSScriptRoot\Comparison Report.html" -Value "
 <html lang=""en"">
@@ -64,15 +70,84 @@ table, td, th {
     padding: 3px;
 }
 td {
+	padding: 3px;
+	border: 1px solid black;
+    text-align:center;
     background-color: #FFC;
 }
+#indication, #action {
+text-align: left;
+}
 </style>
+<script>
+function nameInputFilter() { 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById(""nameInput"");
+  filter = input.value.toUpperCase();
+  table = document.getElementById(""Stats"");
+  tr = table.getElementsByTagName(""tr"");
+  for (i = 1; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName(""td"")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = """";
+      } else {
+        tr[i].style.display = ""none"";
+      }
+    } 
+  }
+}
+function filter(filterid, columnnumber) {
+    var filterValue = document.getElementById(filterid).value;
+    table = document.getElementById(""Stats"");
+    tr = table.getElementsByTagName(""tr"");
+        if (filterValue == ""Discard filter"") {
+            for (i = 1; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName(""td"")[columnnumber];
+                if (td) {
+                tr[i].style.display = """";
+                }  
+            }
+        } else {
+            for (i = 1; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName(""td"")[columnnumber];
+                    if (td) {
+                        if (td.innerText == filterValue) {
+        		tr[i].style.display = """";
+                        } else {
+                        tr[i].style.display = ""none"";
+                        }
+                    } 
+             }
+        }
+}
+</script>
 </head>
 <body>
 <div>
 <h3>Hello.</h3>
 <br>
-<table>
+<table id=""Stats"">
+<tr>
+        <td id=""indication"">
+	        <input type=""text"" id=""nameInput"" onkeyup=""nameInputFilter()"" placeholder=""Search for names..."">
+	    </td>
+        <td>
+            <select id=""OldVersionFilter"" onchange=""filter('OldVersionFilter', 1)"">
+            <option value=""Discard filter"">Discard filter</option>
+            <option value=""Found"">Found</option>
+            <option value=""Not found"">Not found</option>
+            </select>
+	    </td>
+        <td>
+	        <select id=""MD5filter"" onchange=""filter('MD5filter', 2)"">
+            <option value=""Discard filter"">Discard filter</option>
+            <option value=""Match"">Match</option>
+            <option value=""No match"">No match</option>
+            </select>
+	    </td>
+        <td>This cell is so lovely.</td>
+</tr>
 <tr>
         <th>Document name</th>
         <th>Old version</th> 
@@ -80,9 +155,6 @@ td {
         <th>Action</th>
 </tr>" -Encoding UTF8
 #==========Statistics==========#
-#script code
-$path = Select-Folder -description "Specify a path to '# Source documents doc, docx, xls, xlsx' folder you want to compare new documents against"
-Write-Host "Calculating MD5 checksums..."
 Get-ChildItem -Path "$PSScriptRoot\KPD\# Source documents docx, doc, xls, xlsx\*.*" -Include "*.doc*", "*.xls*" | % {
 $fileMD5s += (Get-FileHash -Path $_.FullName -Algorithm MD5).Hash
 $fileNames += $_.Name
@@ -94,6 +166,7 @@ for ($i = 0; $i -lt $fileData[0].Length ; $i++) {
     $searchFor = $path + "\" + $fileData[0][$i]
     $existence = Test-Path -Path $searchFor
     $statFileName = $fileData[0][$i]
+    Start-Sleep -Milliseconds 50
     if ($existence) {
         Write-Host "Comparing" $fileData[0][$i] "against its previous version..."
         Write-Host "Previous version found" -ForegroundColor Green
@@ -101,33 +174,36 @@ for ($i = 0; $i -lt $fileData[0].Length ; $i++) {
         if ($fileData[1][$i] -eq $foundFileMD5) {
 #==========Statistics==========#
 Add-Content -Path "$PSScriptRoot\Comparison Report.html" -Value "<tr>
-        <td>$statFileName</td>
-        <td>Found</td>
-        <td>Match</td>" -Encoding UTF8
+        <td id=""indication"">$statFileName</td>
+        <td><font color=""green""><b>Found</b></font></td>
+        <td><font color=""green""><b>Match</b></font></td>" -Encoding UTF8
 #==========Statistics==========#
         Write-Host "MDs match" -ForegroundColor Green
         Prompt-Overwrite -FileName $fileData[0][$i] -TestPath "$PSScriptRoot\KPD\# Matches against previous version" -FileFullName $fileData[2][$i] -Statistics "# Matches against previous version"
+        $script:MathcesAgainstPreviousVersion += 1
         #Copy-Item -Path $fileData[2][$i] -Destination "$PSScriptRoot\KPD\# Matches against previous version\" 
         } else {
 #==========Statistics==========#
 Add-Content -Path "$PSScriptRoot\Comparison Report.html" -Value "<tr>
-        <td>$statFileName</td>
-        <td>Found</td>
-        <td>No match</td>" -Encoding UTF8
+        <td id=""indication"">$statFileName</td>
+        <td><font color=""green""><b>Found</b></font></td>
+        <td><font color=""red""><b>No match</b></font></td>" -Encoding UTF8
 #==========Statistics==========#
         Write-Host "MDs DO NOT match" -ForegroundColor Red
         Prompt-Overwrite -FileName $fileData[0][$i] -TestPath "$PSScriptRoot\KPD\# Source documents to be translated" -FileFullName $fileData[2][$i] -Statistics "# Source documents to be translated"
+        $script:SourceDocumentsToBeTranslated += 1
         #Copy-Item -Path $fileData[2][$i] -Destination "$PSScriptRoot\KPD\# Source documents to be translated\"
         }
     } else {
 #==========Statistics==========#
 Add-Content -Path "$PSScriptRoot\Comparison Report.html" -Value "<tr>
-        <td>$statFileName</td>
-        <td>Not found</td>
+        <td id=""indication"">$statFileName</td>
+        <td><font color=""red""><b>Not found</b></font></td>
         <td>---</td>" -Encoding UTF8
 #==========Statistics==========#
     Write-Host "Previous version NOT found" -ForegroundColor Red
     Prompt-Overwrite -FileName $fileData[0][$i] -TestPath "$PSScriptRoot\KPD\# Source documents to be translated" -FileFullName $fileData[2][$i] -Statistics "# Source documents to be translated"
+    $script:SourceDocumentsToBeTranslated += 1
     #Copy-Item -Path $fileData[2][$i] -Destination "$PSScriptRoot\KPD\# Source documents to be translated\"
     }
 Write-Host "-----------"
@@ -139,3 +215,7 @@ Add-Content "$PSScriptRoot\Comparison Report.html" "</table>
 $fileNames = @()
 $fileMD5s = @()
 $fileFullNames = @()
+$totalCount = $script:SourceDocumentsToBeTranslated + $script:MathcesAgainstPreviousVersion
+$string = "<h3>Hello.</h3>`r`n<h4>Documents processed: $totalCount</h4>`r`n<h4>Copied to '# Source documents to be translated': $script:SourceDocumentsToBeTranslated</h4>`r`n<h4>Copied to '# Matches against previous version': $script:MathcesAgainstPreviousVersion</h4>"
+(Get-Content -Path "$PSScriptRoot\Comparison Report.html").Replace("<h3>Hello.</h3>", $string) | Set-Content("$PSScriptRoot\Comparison Report.html")
+Invoke-Item "$PSScriptRoot\Comparison Report.html"
