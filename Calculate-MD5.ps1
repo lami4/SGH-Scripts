a5dafb893c13086d76d43fcd289ae9e20cbd824f
lami4 <fazz@mail.ru>
2017-03-13 12:29:53 +0300

    Update Calculate-MD5.ps1

--- Calculate-MD5.ps1
+++ Calculate-MD5.ps1
@@ -19,6 +19,7 @@ Function Input-YesOrNo ($Question, $BoxTitle)
 
 $SelectedFolder = Select-Folder -description "Укажите папку, в которой необходимо снять контрольный суммы файлов."
 if ((Test-Path -Path "$PSScriptRoot\MD5 файлов текущего релиза.txt") -eq $true) {
+$nl = [System.Environment]::NewLine
 Input-YesOrNo  -Question "Список 'MD5 файлов текущего релиза.txt' уже сущетвует. Продолжить?$nl$nl`Да - перезаписать и продолжить исполнение скрипта.$nl`Нет - не перезаписывать и остановить исполнение скрипта.$nl$nl`Если вы не хотите перезаписывать существующий список, но хотите продолжить исполнение скрипта - переместите список из папки, где расположен файл скрипта, в любое удобное для вас место и нажмите 'Да'." -BoxTitle "Список уже существует"
 if ($script:yesNoUserInput -eq 1) {Remove-Item -Path "$PSScriptRoot\MD5 файлов текущего релиза.txt"}
 $script:yesNoUserInput = 0
